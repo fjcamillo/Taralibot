fbb227d445b86202ebdd27a6a4cb13bec55b0d58
fjcamillo <fjcamillo.dev@gmail.com>
2017-02-07 19:35:23 +0800

    url

---
+++ tourismbot/home/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from .views import index
+
+urlpattern = (
+    url(r'^'),
+)
--- tourismbot/tourismbot/settings.py
+++ tourismbot/tourismbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's7)3r@#_aumdsy+(doz8i^=k$pbw66rz=n7*j$d6rrm@3wujr&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'chat'
 ]
 
 MIDDLEWARE = [
