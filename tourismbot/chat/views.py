d314d5d6c50c25bbc7422490bed12db6da9c0260
fjcamillo <fjcamillo.dev@gmail.com>
2017-02-17 17:40:53 +0800

    added chat capability

--- tourismbot/chat/views.py
+++ tourismbot/chat/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.views.decrators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.views import generic
+from django.http import HttpResponse
+import requests
+import json
+import os
+import time
 
-# Create your views here.
+verify_token = '5244680129'
+
+page_access_token = 'EAAIwxSTcnu0BALz1yGvBSgnwXgwEQuv6IVoHmob6VYHni2EqCYSWYdZA3oM9e64tZBbeC5tn94mdvHZAoJzKhwzN9Thj8d3cYHzkSTgsbijEGhhZAh6msbG5i5y1eKX7xq6I3t0QPf8owXKhdbdJZAjLrhvZCoS7ZCHqBTgthRBlwZDZD'
+
+class index(generic.View):
+
+    def get(self, request, *args, **kwargs):
+        if self.request.GET['hub.verify_token'] == verify_token:
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Error', 'Invalid Token')
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return generic.View.dispatch(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        incoming_message = json.loads(self.request.body.decode('utf-8'))
+        for entry in incoming_message['entry']:
+            for message in entry['messaging']:
+                post_facebook_messages(message['sender']['id'], message['message']['text'])
+        return HttpResponse()
+
+def post_facebook_messages(fbid, received_messages):
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token='+page_access_token
+    response_msg = json.dumps({"recipient": {"id":fbid},"message":{"text":received_messages}})
+    print('-----')
+    print(response_msg)
+
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
+    pprint(status.json())
+    return
