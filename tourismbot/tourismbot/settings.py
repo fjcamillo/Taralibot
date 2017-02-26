0d064d8f151fee90b8e17300f4e42ebd6cdbb3eb
fjcamillo <fjcamillo.dev@gmail.com>
2017-02-26 16:23:41 +0800

    added postgresql database

--- tourismbot/tourismbot/settings.py
+++ tourismbot/tourismbot/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+from . import userpass
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,11 +76,26 @@ WSGI_APPLICATION = 'tourismbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+user = userpass.taraliBotDatabaseUsername()
+passw = userpass.taraliBotDatabasePassword()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taralibotdb',
+        'USER': user,
+        'PASSWORD': passw,
+        'HOST': 'localhost',
+        'PORT': ''
     }
+    # 'default': 'postgres://taraliBot:softwareengineering@127.0.0.1:5432/taraliBotDB'
 }
 
 
