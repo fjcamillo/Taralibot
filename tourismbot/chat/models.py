5a33a716391ebcb84e369f2a6cd4d44d51cd334e
fjcamillo <fjcamillo.dev@gmail.com>
2017-03-06 23:22:46 +0800

    creating database

--- tourismbot/chat/models.py
+++ tourismbot/chat/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+import django.contrib.postgres.fields as post
 # Create your models here.
+
+class destinations(models.Model):
+    name = models.CharField(max_length=40)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    services = post.JSONField()
+    
