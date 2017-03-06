1f503aefdaa2294433b0fa4e7a892c8e87891bc6
fjcamillo <fjcamillo.dev@gmail.com>
2017-03-07 02:33:19 +0800

    added models and admin configurations

--- tourismbot/chat/admin.py
+++ tourismbot/chat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.chatusers)
+admin.site.register(models.conversations)
+admin.site.register(models.destinations)
+admin.site.register(models.services)
--- tourismbot/chat/models.py
+++ tourismbot/chat/models.py
@@ -8,5 +8,17 @@ class destinations(models.Model):
     name = models.CharField(max_length=40)
     longitude = models.FloatField()
     latitude = models.FloatField()
-    services = post.JSONField()
-    
+    defaultimage = models.ImageField(null=True)
+    owner = models.CharField(max_length=20)
+
+class services(models.Model):
+    destinations_fk = models.ForeignKey(destinations, on_delete=models.CASCADE)
+    list_services = post.JSONField()
+
+class chatusers(models.Model):
+    facebook_id = models.CharField(max_length=100)
+    facebook_image = models.ImageField(null=True)
+
+class conversations(models.Model):
+    facebook_fk = models.ForeignKey(chatusers, on_delete=models.CASCADE)
+    converse = models.CharField(max_length=320)
