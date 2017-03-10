34454722c7da131d5c2cfdb7a86beeafb83dc107
fjcamillo <fjcamillo.dev@gmail.com>
2017-03-10 15:04:15 +0800

    working on persistent menu

--- tourismbot/chat/admin.py
+++ tourismbot/chat/admin.py
@@ -2,8 +2,43 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+class chatusermodel(admin.ModelAdmin):
+    list_display = ['id', 'facebook_id', 'facebook_image']
 
-admin.site.register(models.chatusers)
-admin.site.register(models.conversations)
-admin.site.register(models.destinations)
-admin.site.register(models.services)
+    class Meta:
+        model = models.chatusers
+
+class destinationmodel(admin.ModelAdmin):
+    list_display = ['id', 'name', 'longitude', 'latitude',
+                    'defaultimage', 'owner', 'description', 'website',
+                    'imageUrls', 'address', 'contact_number',
+                    'image_one','image_two','image_three',
+                    'image_four','image_five']
+    class Meta:
+        model = models.destinations
+
+class servicesmodel(admin.ModelAdmin):
+    list_display = ['id', 'destinations_fk', 'list_services']
+
+    class Meta:
+            model = models.services
+
+class conversationsmodel(admin.ModelAdmin):
+    list_display = ['id','facebook_fk','converse','timestamp']
+
+    class Meta:
+        model = models.conversations
+
+class useraccountmodels(admin.ModelAdmin):
+    list_display = ['id', 'username', 'password',
+                    'email_address', 'facebook_connected_id',
+                    'age', 'image']
+
+    class Meta:
+        model = models.user_accounts
+
+admin.site.register(models.chatusers, chatusermodel)
+admin.site.register(models.conversations, conversationsmodel)
+admin.site.register(models.destinations, destinationmodel)
+admin.site.register(models.services, servicesmodel)
+admin.site.register(models.user_accounts, useraccountmodels)
--- tourismbot/chat/models.py
+++ tourismbot/chat/models.py
@@ -13,6 +13,11 @@ class destinations(models.Model):
     description = models.CharField(max_length=500, default='Description')
     website = models.CharField(max_length=100, default='www.taralibot.xyz')
     imageUrls = post.JSONField(default="{default: none}")
+    image_one = models.ImageField(null=True)
+    image_two = models.ImageField(null=True)
+    image_three = models.ImageField(null=True)
+    image_four = models.ImageField(null=True)
+    image_five = models.ImageField(null=True)
     address = models.CharField(max_length=100, default="PUP Sta.Mesa")
     contact_number = models.CharField(max_length=20, default="0")
 
@@ -24,8 +29,18 @@ class services(models.Model):
 class chatusers(models.Model):
     facebook_id = models.CharField(max_length=100)
     facebook_image = models.ImageField(null=True)
+    facebook_name = models.CharField(max_length=100, default='name')
 
 class conversations(models.Model):
     facebook_fk = models.ForeignKey(chatusers, on_delete=models.CASCADE)
     converse = models.CharField(max_length=320)
     timestamp = models.DateTimeField(auto_now=True)
+
+class user_accounts(models.Model):
+    username = models.CharField(max_length=50, default='username')
+    password = models.CharField(max_length=100, default='passwords')
+    email_address = models.CharField(max_length=50, default='taralibot@gmail.com')
+    facebook_connected_id = models.ForeignKey(chatusers, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    image = models.ImageField(null=True)
+
